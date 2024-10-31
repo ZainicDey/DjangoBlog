2697c5d49fcd0d9dc45aeaa190e0e2240a84cd51
zainic <ihanik.ad@gmail.com>
2024-11-01 00:18:38 +0600

    added

--- website/settings.py
+++ website/settings.py
@@ -88,7 +88,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
