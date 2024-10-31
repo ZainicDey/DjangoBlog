3cd9c8263e4ca91e45745c795dc2906d59efa242
zainic <ihanik.ad@gmail.com>
2024-10-31 22:57:23 +0600

    added

--- website/wsgi.py
+++ website/wsgi.py
@@ -1,5 +1,8 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'website.settings')  # Update 'website' to your project name
+# Set the default settings module for the 'DJANGO_SETTINGS_MODULE' environment variable
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'website.settings')  # Replace 'your_project_name' with your actual project name
+
+# Get the WSGI application for your project
 application = get_wsgi_application()
